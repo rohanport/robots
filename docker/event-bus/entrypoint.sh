f35151689060daf6910e28b8a6635463dca83cb4
Rohan Port <rohan.port@gmail.com>
2023-09-27 19:03:39 +1000

    Added event-bus package

---
+++ docker/event-bus/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Basic entrypoint for Event Bus Docker containers
+
+# Execute the command passed into this entrypoint
+exec "$@"
--- docker/rxinfer/entrypoint.sh
+++ docker/rxinfer/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Basic entrypoint for RxInfer Docker containers
 
-export PATH="$PATH:/julia/bin"
 export JULIA_PROJECT="/rxinfer"
 
 # Execute the command passed into this entrypoint
